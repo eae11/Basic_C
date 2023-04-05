9ba9d9da30d46ece02e72ff0ff93e3e635354351
zyr <1747735542@qq.com>
2023-04-06 00:30:44 +0800

    2023.4.6

--- d2/a_003.c
+++ d2/a_003.c
@@ -4,65 +4,65 @@
 #include <time.h>
 
 void help(void) {
-    printf("*************************\n");
-    printf("* ?????????????\n");
-    printf("* ????????????????\n");
-    printf("* ??????????????,???????????????!\n");
-    printf("* ???????????_???\n");
-    printf("*************************\n");
+  printf("*************************\n");
+  printf("* 输入过程无法退出\n");
+  printf("* 请按所给字母敲击键盘\n");
+  printf("* 按任意键开始测试,按下首字母开始计时!\n");
+  printf("* 输入出错则以_表示\n");
+  printf("*************************\n");
 }
 
 int main() {
-    char ch;
-    char str[51] = "";//???????,????????????26???????  50??????????'\0'
-    int i;
-    int count;
-    time_t start_time, end_time;
+  char ch;
+  char str[51] = "";//字符数组,里面每个元素是26个字母之一  50个最后一个放'\0'
+  int i;
+  int count;
+  time_t start_time, end_time;
+  while (1) {
+    system("cls");
+    help();
+    fflush(stdout);
+    ch = _getch();//输入任意按键
+    srand(time(NULL));
+    for (int i = 0; i < 50; ++i) {
+      str[i] = rand() % 26 + 'a';//保证生成的随机数是26个字母
+    }
+    str[50] = '\0';
+    printf("%s\n", str);//把生成的字母打印出来
+    count = 0;
+    for (int i = 0; i < 50; ++i) {
+      ch = _getch();
+      if (i == 0) {
+        start_time = time(NULL);//打印第一个字母开始计时
+      }
+      if (ch == str[i]) {
+        count++;
+        printf("%c", ch);
+      } else {
+        printf("_");
+      }
+
+    }
+    end_time = time(NULL);
+    printf("\ncount=%d\n", count);
+    printf("正确率 %d %c\n", count * 100 / 50, '%');
+    printf("用时%ld秒\n", end_time - start_time);
+    printf("按空格再玩一次,按esc退出\n");
+    //玩完一次游戏后进入循环 按空格再玩一次,按esc退出
     while (1) {
-        system("cls");
-        help();
-        fflush(stdout);
-        ch = _getch();//??????????
-        srand(time(NULL));
-        for (int i = 0; i < 50; ++i) {
-            str[i] = rand() % 26 + 'a';//???????????????26?????
-        }
-        str[50] = '\0';
-        printf("%s\n", str);//?????????????????
-        count = 0;
-        for (int i = 0; i < 50; ++i) {
-            ch = _getch();
-            if (i == 0) {
-                start_time = time(NULL);//?????????????????
-            }
-            if (ch == str[i]) {
-                count++;
-                printf("%c", ch);
-            } else {
-                printf("_");
-            }
 
-        }
-        end_time = time(NULL);
-        printf("\ncount=%d\n", count);
-        printf("????? %d %c\n", count * 100 / 50, '%');
-        printf("???%ld??\n", end_time - start_time);
-        printf("????????????,??esc???\n");
-        //?????????????????? ????????????,??esc???
-        while (1) {
-
-            ch = _getch();
-            if (ch == ' ') {
-                break;//??????????
-            }
-            if (ch == 27)//esc
-            {
-                return 0;
-            }
-
-        }
+      ch = _getch();
+      if (ch == ' ') {
+        break;//继续外层循环
+      }
+      if (ch == 27)//esc
+      {
+        return 0;
+      }
 
     }
 
-    return 0;
+  }
+
+  return 0;
 }
